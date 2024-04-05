4dc373e94e311ccdb145b5f19692e1d741b9b7c6
Luis <luisgarcesaraujo@gamil.com>
2024-04-05 13:37:04 -0300

    initial commit

---
+++ nlw-unite-java-main/src/main/java/rocketseat/com/passin/domain/attendee/exceptions/AttendeeNotFoundException.java
@@ -0,0 +1,6 @@
+package rocketseat.com.passin.domain.attendee.exceptions;
+
+public class AttendeeNotFoundException  extends RuntimeException{
+    public AttendeeNotFoundException(String message){super(message);
+    }
+}
